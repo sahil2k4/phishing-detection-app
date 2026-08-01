ecb2a56ad753f3280e1521f314a70148c0193068
sahil2k4 <sahilbopche3@gmail.com>
2026-08-01 20:09:49 +0530

    Improve Render startup compatibility

--- app.py
+++ app.py
@@ -378,7 +378,7 @@ def predict():
 if __name__ == "__main__":
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
